3d57b527b3ff113295820035dff8a090b74f400a
EWintersS <abhinavsinghstar8@gmail.com>
2025-10-26 23:38:43 +0530

    Update loginServlet.java

--- loginServlet.java
+++ loginServlet.java
@@ -1,1 +1,22 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String user = request.getParameter("username");
+        String pass = request.getParameter("password");
+        if (user.equals("admin") && pass.equals("12345")) {
+            out.println("<html><body>");
+            out.println("<h2>Welcome, " + user + "!</h2>");
+            out.println("</body></html>");
+        } else {
+            out.println("<html><body>");
+            out.println("<h3>Invalid credentials. Try again.</h3>");
+            out.println("</body></html>");
+        }
+    }
+}
 
